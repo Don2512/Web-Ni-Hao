5d8054bf887c5c3b02689aa1c28495f301e9fa05
Don <daoduylongpy@gamil.com>
2024-04-08 00:19:47 +0700

    initEND

---
+++ FE/src/App.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Don CUTE</h1>
+    </div>
+  );
+}
+
+export default App;
--- FE/src/index.js
+++ FE/src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import "bootstrap/dist/css/bootstrap.min.css";
-// import "bootstrap/dist/js/bootstrap.bundle.min";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-     <h1>Don</h1>
-     <button type="button" class="btn btn-primary">Primary</button>
-     <button type="button" class="btn btn-outline-primary">Primary</button>
+    <App />
   </React.StrictMode>
 );
 
