3d97315d78006e8ebfd2b45b81e3e38739f0c787
Don <daoduylongpy@gamil.com>
2024-04-08 13:33:27 +0700

    a

--- FE/src/App.js
+++ FE/src/App.js
@@ -1,11 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import SoTayScreen from "./Screen/soTayScreen";
-import BaiTapScreen from "./Screen/baiTapScreen";
-import TrangChuScreen from "./Screen/trangChu";
 import Data from "./Data/data";
 function App() {
-  const baseURL = "/Don2512/Web-Ni-Hao";
   return <Data />;
 }
 
--- FE/src/Data/data.js
+++ FE/src/Data/data.js
@@ -1,50 +1,30 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import axios from "axios";
+// import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes, Link } from 'react-router-dom';
+// import axios from "axios"; // Import if needed
 
 import SoTayScreen from "../Screen/soTayScreen";
 import BaiTapScreen from "../Screen/baiTapScreen";
 import TrangChuScreen from "../Screen/trangChu";
-function Data() {
-  const [soTayData, setSoTayData] = useState([]);
-  const [baiTapData, setBaiTapData] = useState([]);
-  const baseURL = "/Don2512/Web-Ni-Hao";
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(
-          "https://sheets.googleapis.com/v4/spreadsheets/1fSWPkmCz8zNjGquFN0yIOpP_p578t0ij-RERcHxoDr0/values/soTay?key=AIzaSyAQj26vYu-gkIeBTTiwinN3VPEYGfLQ-Y8"
-        );
-        console.log(response);
-        setSoTayData(
-          response.data.values
-            .splice(1)
-            .sort((a, b) => a[4].localeCompare(b[4]))
-        );
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
 
-      try {
-        const response = await axios.get(
-          "https://sheets.googleapis.com/v4/spreadsheets/1fSWPkmCz8zNjGquFN0yIOpP_p578t0ij-RERcHxoDr0/values/baiTap?key=AIzaSyAQj26vYu-gkIeBTTiwinN3VPEYGfLQ-Y8"
-        );
-        console.log(response);
-        setBaiTapData(response.data.values.splice(1));
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-    fetchData();
-  }, []);
+function Data() {
   return (
-    <Router>
+    <>
+
+    <HashRouter> 
+      <h1>Duy Long</h1>
+      <nav>
+          <Link class="btn btn-primary btn-lg" to="/">Trang Chủ</Link>
+          <Link class="btn btn-primary btn-lg" to="/soTay">Sổ Tay</Link>
+          <Link class="btn btn-primary btn-lg" to="/baiTap">Bài Tập</Link>
+        </nav>
       <Routes>
-        <Route path={baseURL + "/soTay"} element={<SoTayScreen />} />
-        <Route path="/baiTap" element={BaiTapScreen} />
-        <Route path="/" element={TrangChuScreen} />
+        <Route path="/soTay" element={<SoTayScreen />} />
+        <Route path="/baiTap" element={<BaiTapScreen />} />
+        <Route path="/" element={<TrangChuScreen />} />
       </Routes>
-    </Router>
+    </HashRouter>
+    </>
   );
 }
 
